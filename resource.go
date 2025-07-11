06af54b2a9855ed93bfacc5e3661e9719a73c22e
jthmath <jthmath@vip.163.com>
2025-07-11 17:42:58 +0800

    save

--- resource.go
+++ resource.go
@@ -18,6 +18,9 @@ type Resource struct {
 
 func (r *Resource) GetWinStr() (*uint16, error) {
 	if r.s == "" {
+		// 制造一个悬挂指针
+		// 不过这是安全的，因为 uint16 范围的地址肯定不会是一个有效的指针
+		// WinAPI 函数不会误解
 		return (*uint16)(unsafe.Pointer(uintptr(r.n))), nil
 	} else {
 		return syscall.UTF16PtrFromString(r.s)
